f5d73ea8472867b268bb22eaa5a1f9b51e9c45ed
Antonin Bures <antoninbures@gmail.com>
2023-12-02 10:30:16 +0100

    fancybox

---
+++ projects/kampus/js/fancybox-definition.js
@@ -0,0 +1,60 @@
+/* Vypravna galerie */
+import { Fancybox } from "./fancybox.js";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+document.addEventListener('DOMContentLoaded', function () {
+    var galleryItems = document.querySelectorAll('.gallery-item');
+  
+    // Process gallery items
+    processGalleryItems();
+  
+    function processGalleryItems() {
+      galleryItems.forEach(function (item, index) {
+        var h4 = item.querySelector('.gallery-item-title');
+        var p = item.querySelector('.gallery-item-description');
+  
+        // If elements are found, insert values
+        if (h4 && p) {
+          // Set data-caption attribute on the anchor element
+          var parentElement = item.querySelector('.gallery-lightbox-wrapper');
+          if (parentElement && parentElement.tagName === 'A') {
+            var title = h4.textContent.trim();
+            var description = p.textContent.trim();
+  
+            parentElement.setAttribute('data-caption', `<h3>${title}</h3><p>${description}</p>`);
+          } else {
+            console.error('Parent of the image is not an anchor element for', item);
+          }
+        }
+        
+        // Additional logic for copying src attribute to the direct parent's href
+        var img = item.querySelector('img');
+        if (img) {
+          var parentElement = item.querySelector('.gallery-lightbox-wrapper');
+          if (parentElement && parentElement.tagName === 'A') {
+            var imgSrc = img.src;
+            parentElement.href = imgSrc;
+          } else {
+            console.error('Parent of the image is not an anchor element for', item);
+          }
+        }
+      });
+      Fancybox.bind('[data-fancybox]', {
+          contentClick: false,
+          Images: {
+              Panzoom: {
+                  maxScale: 0,
+              },
+              
+          },
+          Toolbar: {
+              display: {
+                  left: '',
+                  middle: '',
+                  right: ["close"],
+              },
+          },
+      })
+    }
+});
+/* Vypravna galerie */
